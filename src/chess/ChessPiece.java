ae7364d6cd453ec29fdaf2a868b1884703e1ab1f
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-09-29 16:00:40 -0300

    feat: adiciona a classe "ChessPiece" usada para instanciar as pecas do xadrez

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,22 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+
+/**
+ * This class is responsible for instantiating chess pieces.
+ */
+public class ChessPiece extends Piece {
+  private Color color;
+
+  public ChessPiece(Board board, Color color) {
+    super(board);
+    this.color = color;
+  }
+
+  public Color getColor() {
+    return color;
+  }
+
+}
