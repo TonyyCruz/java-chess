80377390676bbe768e1293ec1a7c2bec2d3ce6eb
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-09-29 13:13:51 -0300

    feat: adiciona a classe "Piece" responsavel por representar as pecas do xadrez

---
+++ src/boardgame/Board.java
@@ -0,0 +1,37 @@
+package boardgame;
+
+/**
+ * This is responsible for managing the board and showing the position of the pieces.
+ */
+public class Board {
+  private int rows;
+  private int columns;
+  private Piece[][] pieces;
+
+  /**
+   * This start the board with the rows and columns received.
+   */
+  public Board(int rows, int columns) {
+    this.rows = rows;
+    this.columns = columns;
+    pieces = new Piece[rows][columns];
+  }
+
+  public int getRows() {
+    return rows;
+  }
+
+  public void setRows(int rows) {
+    this.rows = rows;
+  }
+
+  public int getColumns() {
+    return columns;
+  }
+
+  public void setColumns(int columns) {
+    this.columns = columns;
+  }
+
+
+}
