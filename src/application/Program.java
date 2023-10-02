8959926080f3045dee99ccaa121668dac8e9c59e
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-10-02 16:48:39 -0300

    chore: implemena logica inicial para funcionamento do jogo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Scanner;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 /**
  * This class is the main class. It's responsible all the user interaction and program execution.
@@ -11,9 +14,23 @@ public class Program {
    * The main class.
    */
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     ChessMatch chessMatch = new ChessMatch();
 
-    Ui.printBoard(chessMatch.getPieces());
+    while (true) {
+      Ui.printBoard(chessMatch.getPieces());
+      System.out.println();
+      System.out.print("Source: ");
+      ChessPosition source = Ui.readChessPosition(sc);
+
+      System.out.println();
+      System.out.print("Target: ");
+      ChessPosition target = Ui.readChessPosition(sc);
+
+      ChessPiece capturedPiece = chessMatch.performChessMovie(source, target);
+
+    }
+
 
     // System.out.println(new ChessPosition('h', 8));
   }
--- src/application/Ui.java
+++ src/application/Ui.java
@@ -1,11 +1,33 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import chess.ChessPiece;
+import chess.ChessPosition;
 
 /**
  * This shows the chess game in the screen.
  */
 public class Ui {
+
+  /**
+   * This receive a string with a chess position, separates the position into 'char' and 'int' and
+   * returns the stance of a ChessPosition.
+   */
+  public static ChessPosition readChessPosition(Scanner sc) {
+    try {
+      String in = sc.next();
+
+      char column = in.charAt(0);
+      int row = Integer.parseInt(in.substring(1));
+
+      return new ChessPosition(column, row);
+
+    } catch (RuntimeException e) {
+      throw new InputMismatchException("Position error: The position need be from 'a1' to 'h8'.");
+    }
+  }
+
   /**
    * This print a board with the current state of the game.
    */
